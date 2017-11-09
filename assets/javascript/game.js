4db16e934e32a211b78c1e06d3221bd6e61d504f
Abby Thoresen <akthoresen39@gmail.com>
2017-11-08 22:35:04 -0700

    yay i did it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,70 @@
 
-// Creates an array that lists out all of the options (Letter computer id thinking of). do i chose letter?
-    var computerChoices = ["c", "a", "t"];
+// Creates an array that lists out all of the options (Letter computer is thinking of)
+    var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    // Creating variables to hold the number of wins, losses, guesses left and Your guesses so far. They start at 0.
+
+// Variables - Creating variables to hold the number of wins, losses, guesses left, guesses so far and user guess set as null.
     var wins = 0;
     var losses = 0;
-    var guessesLeft = 10;
-    var guessesSofar = [];
+    var guessesLeft = 9;
+    var guessesSoFar = [];
+    var userGuess = null;
 
 // Randomly chooses a choice from the options array. This is the Computer's guess.
-      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-      console.log ("This is the computerGuess " + computerGuess)
+    var letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+    console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
 
-    // This function is run whenever the user presses a key.
-     document.onkeyup = function(event) {
-     console.log (guessesSofar)
-      // Determines which key was pressed.
-      var userGuess = event.key;
-      console.log (userGuess)
-      console.log (computerGuess)
-      // userGuess - wins -  if and else if statements
-      if (userGuess === computerGuess){
-      wins = wins +1
-      console.log (wins)
-      }
+// This function is run whenever the user presses a key.
+    document.onkeyup = function(event) {
 
 
-      else if (userGuess !== computerGuess && guessesLeft > 0){
-      guessesLeft = guessesLeft -1
-      guessesSofar.push(userGuess)
-      }
-       console.log (guessesSofar)
-
-    // userGuess -losses - if statement
-     if(userGuess !== computerGuess){
-        losses = losses +1
-      }
-      console.log (losses)
+// This determines which key was pressed. stores in userGuess as null :)
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 
-        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/guesses left/ your guesses so far
-        var html =
-          "<p>You chose: " + userGuess + "</p>" +
-          "<p>The computer chose: " + computerGuess + "</p>" +
-          "<p>wins: " + wins + "</p>" +
-          "<p>losses: " + losses + "</p>" +
-          "<p>guesses left: " + guessesLeft + "</p>";
-          "<p>guesses left: " + guessesSofar + "</p>";
-        // Set the inner HTML contents of the #game div to our html string
-        document.querySelector("#game").innerHTML = html;
-      }
-    //*};
+// User Guess is added to GuessesSoFar - make not sure already picked - alphabet consistancy
+    if (guessesSoFar.indexOf(userGuess) < 0 && alphabetLetters.indexOf(userGuess) >= 0) {
+    guessesSoFar[guessesSoFar.length]=userGuess;
+    // if new then decrease by 1
+    guessesLeft--;
+    }
+
+
+// if letterToBeGuessed is same as userGuess then its a winner / reset guessesLeft to 9 / empty guessesSoFar / new random computer choice
+    if (letterToBeGuessed == userGuess) {
+    wins++;
+    console.log("You lucky duck, you WON!");
+    guessesLeft = 9;
+    guessesSoFar = [];
+    letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+    console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+    }
+
+
+
+// if GuessesLeft turns 0 then its a Loss / reset guessesLeft to 9 / empty guessesSoFar / new random computer choice
+    if (guessesLeft == 0) {
+    losses++;
+    console.log("Sorry you lost. Try again :)");
+    guessesLeft = 9;
+    guessesSoFar = [];
+    letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+    console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+    }
+
+
+// Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/guesses left/ your guesses so far
+          var html =
+          "<p>Guess what letter I'm thinking of?</p>"+
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Losses: " + losses + "</p>" +
+          "<p>Guesses left: " + guessesLeft + "</p>" +
+          "<p>Your guesses so far: " + guessesSoFar + "</p>";
+          // Set the inner HTML contents of the #game div to my index.html
+           document.querySelector("#game").innerHTML = html;
+  }
+
+//The end - whew wee
+
+
+
