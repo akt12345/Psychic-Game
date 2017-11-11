4dcdf1109cffb1dc96e36fbe2a1efa70ab40420c
Abby Thoresen <akthoresen39@gmail.com>
2017-11-10 19:46:20 -0700

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-// Creates an array that lists out all of the options (Letter computer is thinking of)
+// Creates an array that lists out all of the options (Letter computer is thinking of) call this alphabetLetters
     var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
